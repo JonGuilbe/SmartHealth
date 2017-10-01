f4d237871bdc101293d0ea8afa09329aabd8fe2d
JonGuilbe <jguilbe@knights.ucf.edu>
2017-10-01 15:10:20 -0400

    Updated gitignore, Set up packages for DB, created example DB Model/View/Controller

---
+++ Models/postgresContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+
+namespace SmartHealth
+{
+    public partial class postgresContext : DbContext
+    {
+        public virtual DbSet<Test> Test { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql(@Startup.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasPostgresExtension("adminpack");
+
+            modelBuilder.Entity<Test>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .HasColumnName("ID")
+                    .ValueGeneratedNever();
+            });
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 namespace SmartHealth
 {
@@ -17,11 +19,16 @@ namespace SmartHealth
         }
 
         public IConfiguration Configuration { get; }
+        public static string ConnectionString { get; private set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            //services.AddDbContext<postgresContext>(options =>
+            //options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+            ConnectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<postgresContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
