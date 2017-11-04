64f5144041505ff638b392bb00a6db140bdab9a7
Jonathan Guilbe <jguilbe@knights.ucf.edu>
2017-11-04 15:12:15 -0400

    Modified AccountController, created new ApplicationUsers, removed warning from DBContext

--- Models/postgresContext.cs
+++ Models/postgresContext.cs
@@ -13,7 +13,7 @@ namespace SmartHealth
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+
                 optionsBuilder.UseNpgsql(@Startup.ConnectionString);
             }
         }
