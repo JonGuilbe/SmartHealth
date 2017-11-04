6787fc4dce96c09d3d7a4ab795a591e2b1b7ca46
Jonathan Guilbe <jguilbe@knights.ucf.edu>
2017-11-04 16:02:25 -0400

    Updated ApplicationDbContext to include Appointments in the Database

--- Data/Migrations/20171104195220_justtesting.Designer.cs
+++ Data/Migrations/20171104195220_justtesting.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace SmartHealth.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171104195220_justtesting")]
+    partial class justtesting
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
