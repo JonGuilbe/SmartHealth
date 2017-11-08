a28411d947b3457fc4f6b2619992f3bfafb71901
Jonathan Guilbe <jguilbe@knights.ucf.edu>
2017-11-08 17:06:31 -0500

    Fix Email from IdentityUser being overridden, breaking the registration process

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -11,7 +11,6 @@ namespace SmartHealth.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Email { get; set; }
         
     }
 }
