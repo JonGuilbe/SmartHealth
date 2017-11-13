d38aeeacf4adc4dc36b6f3ecc30fd0ec383ab11e
Jonathan Guilbe <jguilbe@knights.ucf.edu>
2017-11-13 10:08:32 -0500

    Created Doctor & Patient controllers, Home pages for each.

---
+++ Controllers/DoctorController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SmartHealth.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace SmartHealth.Controllers 
+
+{
+    public class DoctorController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
